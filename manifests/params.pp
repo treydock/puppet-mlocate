8a88910a02e1af2a5681630e96b8e7d3a73bc074
Jonathan Araña Cruz <jonhattan@faita.net>
2017-03-29 10:24:14 +0200

    Add fuse.glusterfs to PRUNEFS.

--- manifests/params.pp
+++ manifests/params.pp
@@ -23,9 +23,9 @@ class mlocate::params {
   $prunefs = [
     '9p', 'afs', 'anon_inodefs', 'auto', 'autofs', 'bdev', 'binfmt_misc',
     'cgroup', 'cifs', 'coda', 'configfs', 'cpuset', 'debugfs', 'devpts',
-    'ecryptfs', 'exofs', 'fuse', 'fusectl', 'gfs', 'gfs2', 'hugetlbfs',
-    'inotifyfs', 'iso9660', 'jffs2', 'lustre', 'mqueue', 'ncpfs', 'nfs',
-    'nfs4', 'nfsd', 'pipefs', 'proc', 'ramfs', 'rootfs', 'rpc_pipefs',
+    'ecryptfs', 'exofs', 'fuse', 'fusectl', 'fuse.glusterfs', 'gfs', 'gfs2',
+    'hugetlbfs', 'inotifyfs', 'iso9660', 'jffs2', 'lustre', 'mqueue', 'ncpfs',
+    'nfs', 'nfs4', 'nfsd', 'pipefs', 'proc', 'ramfs', 'rootfs', 'rpc_pipefs',
     'securityfs', 'selinuxfs', 'sfs', 'sockfs', 'sysfs', 'tmpfs', 'ubifs',
     'udf', 'usbfs',
   ]
